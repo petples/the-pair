9d2c6012b2d3afcef7fee35b5aa9df35c53eda9f
timwuhaotian <gosingk@gmail.com>
2026-03-24 16:20:33 +0800

    fix(renderer): add missing window.api types to env.d.ts

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,1 +1,32 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  api: {
+    pair: {
+      create: (input: any) => Promise<any>
+      assignTask: (pairId: string, input: any) => Promise<any>
+      updateModels: (pairId: string, input: any) => Promise<any>
+      stop: (pairId: string) => Promise<any>
+      retryTurn: (pairId: string) => Promise<any>
+      list: () => Promise<any>
+      getMessages: (pairId: string) => Promise<any>
+      getState: (pairId: string) => Promise<any>
+      humanFeedback: (pairId: string, approved: boolean) => Promise<any>
+      onCreated: (callback: (data: any) => void) => Promise<any>
+      onStopped: (callback: (data: any) => void) => Promise<any>
+      onMessage: (callback: (data: any) => void) => Promise<any>
+      onState: (callback: (data: any) => void) => Promise<any>
+    }
+    config: {
+      getModels: () => Promise<any>
+      getProviders: () => Promise<any>
+      read: () => Promise<any>
+      openFile: () => Promise<any>
+      getVersion: () => Promise<string>
+    }
+    file: {
+      listFiles: (options: any) => Promise<any>
+      parseMentions: (pairId: string, spec: string) => Promise<any>
+    }
+  }
+}
